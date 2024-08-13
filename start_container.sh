71e2290862770b5811384a751b44490b1a734c64
EliyaS <165130354+Magiciian@users.noreply.github.com>
2024-08-13 22:23:38 +0530

    add the files for code deploy

---
+++ start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the image from docker hub
+docker pull eliyasankark/pyaws-app:tagname
+
+# Run the docker image as a container
+docker run -d -p 5002:5002/eliyasankark/pyaws-app:tagname
