5ebf0a8a4fd33d210d339dea712fe3ce7397b686
EliyaS <165130354+Magiciian@users.noreply.github.com>
2024-08-15 14:36:21 +0530

    Update start_container.sh

--- AWS/start_container.sh
+++ AWS/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the image from docker hub
-docker pull eliyasankark/pyaws-app:tagname
+docker pull eliyasankark/pyaws-app
 
 # Run the docker image as a container
-docker run -d -p 5002:5002/eliyasankark/pyaws-app:tagname
+docker run -d -p 5002:5002/eliyasankark/pyaws-app
