cf5f31113ff35b3a51100d4d64b085780d89387b
EliyaS <165130354+Magiciian@users.noreply.github.com>
2024-08-16 16:33:10 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Pull the image from docker hub
+# Pull the image from Docker Hub
 docker pull eliyasankark/pyaws-app
 
-# Run the docker image as a container
-docker run -d -p 5002:5002/eliyasankark/pyaws-app
+# Run the Docker image as a container
+docker run -d -p 5002:5002 eliyasankark/pyaws-app
