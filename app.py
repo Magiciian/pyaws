d8398321ac0a3d28d382fba9d8064a12b335e556
EliyaS <165130354+Magiciian@users.noreply.github.com>
2024-08-28 19:16:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,4 +105,4 @@ def index():
                            stopped_instances=stopped_instances)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5003)
+    app.run(host='0.0.0.0', port=5003) 
